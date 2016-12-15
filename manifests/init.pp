534466ee9535a31ef754d61b16d24bdb686490b4
Nick Miller <nick.miller@onyxpoint.com>
2016-12-15 15:41:12 -0500

    Fixed up comments and removed all validation functions

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,21 +1,16 @@
-# == Class: openscap
-#
 # This class installs SCAP content and the associated tools.
 # It is mostly based on the scap-security-guide open source project
 # with several customizations for SIMP.
 #
-# == Authors
-#
-# * Ralph Wright <mailto:rwright@onyxpoint.com>
+# @author Ralph Wright <mailto:rwright@onyxpoint.com>
 #
 class openscap(
   Boolean $enable_schedule = false
 ){
 
-  validate_bool($enable_schedule)
-
   if $enable_schedule { include 'openscap::schedule' }
 
-  package { 'openscap-utils': ensure => 'latest' }
+  package { 'openscap-utils':      ensure => 'latest' }
   package { 'scap-security-guide': ensure => 'latest' }
+
 }
--- manifests/schedule.pp
+++ manifests/schedule.pp
@@ -1,14 +1,7 @@
-# == Class: openscap::schedule
-#
 # This class allows you to set a schedule for openscap to run a check
 # on your system via cron.
 #
-# == Parameters
-#
-# [*scap_profile*]
-#   Type: String
-#   Default: "xccdf_org.ssgproject.content_profile_stig-rhel${::operatingsystemmajrelease}-server-upstream"
-#
+# @param scap_profile
 #   Should be the name of the profile with the conent.
 #    Valid RHEL/CentOS 6 Choices:
 #      * xccdf_org.ssgproject.content_profile_test
@@ -28,85 +21,48 @@
 #      * xccdf_org.ssgproject.content_profile_stig-rhel7-server-upstream
 #
 #
-# [*ssg_base_dir*]
-#   Type: Absolute Path
-#   Default: "/usr/share/xml/scap/ssg/content"
-#
+# @param ssg_base_dir
 #   The starting directory for all SSG content. Change this if you want to
 #   install your own SSG profiles.
 #
-# [*ssg_data_stream*]
+# @param ssg_data_stream
 #   Type: XML file under $ssg_base_dir
-#   Default: "ssg-rhel${::operatingsystemmajrelease}-ds.xml"
-#
 #   The data stream XML file to use for your system scan. This must be a file
 #   under $ssg_base_dir.
 #
-# [*fetch_remote_resource*]
-#   Type: Boolean
-#   Default: false
-#
+# @param fetch_remote_resource
 #   If true, download remote content referenced by XCCDF.
 #
-# [*logdir*]
-#   Type: Absolute Path
-#   Default: '/var/log/openscap'
-#
+# @param logdir
 #   Specifies output location.  Default is /var/log/openscap
 #
-# [*rotate_logs*]
-#   Type: Boolean
-#   Default: true
-#
+# @param rotate_logs
 #   If true, use logrotate to rotate the output logs.
 #
-# [*minute*]
-# [*hour*]
-# [*monthday*]
-# [*month*]
-# [*weekday*]
+# @param minute
+# @param hour
+# @param monthday
+# @param month
+# @param weekday
 #
-# == Authors
-#  * Ralph Wright <rwright@onyxpoint.com>
-#  * Trevor Vaughan <tvaughan@onyxpoint.com>
-#  * Kendall Moore <kmoore@keywcorp.com>
+# @author Ralph Wright <rwright@onyxpoint.com>
+# @author Trevor Vaughan <tvaughan@onyxpoint.com>
+# @author Kendall Moore <kmoore@keywcorp.com>
 #
 class openscap::schedule (
-  String                     $scap_profile           = "xccdf_org.ssgproject.content_profile_stig-rhel${::operatingsystemmajrelease}-server-upstream",
-  Stdlib::Unixpath           $ssg_base_dir           = '/usr/share/xml/scap/ssg/content',
-  Pattern[/^.+\.xml$/]       $ssg_data_stream        = "ssg-rhel${::operatingsystemmajrelease}-ds.xml",
-  Boolean                    $fetch_remote_resources = false,
-  Stdlib::Unixpath           $logdir                 = '/var/log/openscap',
-  Boolean                    $rotate_logs            = simplib::lookup('simp_options::logrotate', { 'default_value' => false}),
-  Variant[Enum['*'],Integer]  $minute                 = 30,
-  Variant[Enum['*'],Integer]  $hour                   = 1,
-  Variant[Enum['*'],Integer]  $monthday               = '*',
-  Variant[Enum['*'],Integer]  $month                  = '*',
-  Variant[Enum['*'],Integer]  $weekday                = 1
+  Openscap::Profile                $scap_profile           = "xccdf_org.ssgproject.content_profile_stig-rhel${::operatingsystemmajrelease}-server-upstream",
+  Stdlib::Unixpath                 $ssg_base_dir           = '/usr/share/xml/scap/ssg/content',
+  Pattern[/^.+\.xml$/]             $ssg_data_stream        = "ssg-rhel${::operatingsystemmajrelease}-ds.xml",
+  Boolean                          $fetch_remote_resources = false,
+  Stdlib::Unixpath                 $logdir                 = '/var/log/openscap',
+  Boolean                          $logrotate              = simplib::lookup('simp_options::logrotate', { 'default_value' => false}),
+  Variant[Enum['*'],Integer[0,59]] $minute                 = 30,
+  Variant[Enum['*'],Integer[0,23]] $hour                   = 1,
+  Variant[Enum['*'],Integer[1,31]] $monthday               = '*',
+  Variant[Enum['*'],Integer[1,12]] $month                  = '*',
+  Variant[Enum['*'],Integer[0,7]]  $weekday                = 1
 ) {
-  include 'openscap'
-
-  unless $minute == '*' { validate_between($minute,0,59) }
-  unless $hour == '*' { validate_between($hour,0,23) }
-  unless $monthday == '*' { validate_between($monthday,1,31) }
-  unless $month == '*' { validate_between($month,1,12) }
-  unless $weekday == '*' { validate_between($weekday,0,7) }
-  $valid_profiles = [
-    'xccdf_org.ssgproject.content_profile_test',
-    'xccdf_org.ssgproject.content_profile_CS2',
-    'xccdf_org.ssgproject.content_profile_common',
-    'xccdf_org.ssgproject.content_profile_server',
-    'xccdf_org.ssgproject.content_profile_stig-rhel6-server-upstream',
-    'xccdf_org.ssgproject.content_profile_usgcb-rhel6-server',
-    'xccdf_org.ssgproject.content_profile_rht-ccp',
-    'xccdf_org.ssgproject.content_profile_CSCF-RHEL6-MLS',
-    'xccdf_org.ssgproject.content_profile_C2S',
-    'xccdf_org.ssgproject.content_profile_test',
-    'xccdf_org.ssgproject.content_profile_rht-ccp',
-    'xccdf_org.ssgproject.content_profile_common',
-    'xccdf_org.ssgproject.content_profile_stig-rhel7-server-upstream'
-  ]
-  validate_array_member($scap_profile,$valid_profiles)
+  include '::openscap'
 
   file { $logdir:
     ensure => directory,
@@ -127,8 +83,8 @@ class openscap::schedule (
     ]
   }
 
-  if $rotate_logs {
-    include 'logrotate'
+  if $logrotate {
+    include '::logrotate'
 
     logrotate::add { 'openscap':
       log_files     => [ "${logdir}/*.xml" ],
