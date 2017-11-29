6ba75ac327928e11c5748a6aac58977157a0b725
Brandon Riden <brandon.riden@onyxpoint.com>
2017-11-29 12:58:22 -0500

    updated to use hiera data in modules to fix issue with profile enum

--- types/profile.pp
+++ types/profile.pp
@@ -1,15 +1,1 @@
-type Openscap::Profile = Enum[
-  'xccdf_org.ssgproject.content_profile_test',
-  'xccdf_org.ssgproject.content_profile_CS2',
-  'xccdf_org.ssgproject.content_profile_common',
-  'xccdf_org.ssgproject.content_profile_server',
-  'xccdf_org.ssgproject.content_profile_stig-rhel6-server-upstream',
-  'xccdf_org.ssgproject.content_profile_usgcb-rhel6-server',
-  'xccdf_org.ssgproject.content_profile_rht-ccp',
-  'xccdf_org.ssgproject.content_profile_CSCF-RHEL6-MLS',
-  'xccdf_org.ssgproject.content_profile_C2S',
-  'xccdf_org.ssgproject.content_profile_test',
-  'xccdf_org.ssgproject.content_profile_rht-ccp',
-  'xccdf_org.ssgproject.content_profile_common',
-  'xccdf_org.ssgproject.content_profile_stig-rhel7-server-upstream'
-]
+type Openscap::Profile = Pattern[/xccdf_[^_]+_profile_.+/]
