7a24c469858b5ec3d961ea064a0ab5e4b0d3e52c
Michael Morrone <michael.morrone@onyxpoint.com>
2018-07-18 10:45:47 -0400

    Removed testing of logrotate from schedule spec test

--- spec/classes/schedule_spec.rb
+++ spec/classes/schedule_spec.rb
@@ -1,20 +1,17 @@
 require 'spec_helper'
 
-file_content_7 = "/usr/bin/systemctl restart rsyslog > /dev/null 2>&1 || true"
-file_content_6 = "/sbin/service rsyslog restart > /dev/null 2>&1 || true"
+shared_examples 'a working module' do
+  it { is_expected.to compile.with_all_deps }
+  it { is_expected.to create_class('openscap::schedule') }
+  it { is_expected.to create_class('openscap') }
+
+  it { is_expected.to create_file('/var/log/openscap') }
+  it { is_expected.to create_cron('openscap') } 
+end
 
 describe 'openscap::schedule' do
   context 'supported operating systems' do
     on_supported_os.each do |os, os_facts|
-
-      shared_examples 'a working module' do
-        it { is_expected.to compile.with_all_deps }
-        it { is_expected.to create_class('openscap::schedule') }
-        it { is_expected.to create_class('openscap') }
-
-        it { is_expected.to create_file('/var/log/openscap') }
-        it { is_expected.to create_cron('openscap') }
-      end
 
       context "on #{os}" do
         context 'with a valid environment' do
@@ -56,13 +53,9 @@ describe 'openscap::schedule' do
 
             it_behaves_like 'a working module'
             it { is_expected.to create_class('logrotate') }
-            it { is_expected.to create_logrotate__rule('openscap') }
-
-            if os_facts[:operatingsystemmajrelease].to_s < '7'
-              it { is_expected.to create_file('/etc/logrotate.d/openscap').with_content(/#{file_content_6}/)}
-            else
-              it { is_expected.to create_file('/etc/logrotate.d/openscap').with_content(/#{file_content_7}/)}
-            end
+            it { is_expected.to create_logrotate__rule('openscap').with(
+              { :log_files => [ '/var/log/openscap/*.xml' ] }
+            )  }
           end
 
           context 'when the specified ssg_base_dir is not found' do
